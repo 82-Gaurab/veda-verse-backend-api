60561ba2006915faef9935a69028561a2dc2b359
GAURAB <gaurabkhadka4082@gmail.com>
2026-01-30 12:30:08 +0545

    Additional configuration

--- src/index.ts
+++ src/index.ts
@@ -14,11 +14,14 @@ dotenv.config();
 const app: Application = express();
 let corsOptions = {
   origin: ["http://localhost:3000", "http://localhost:3005"],
+  optionsSuccessStatus: 200,
+  credentials: true,
 };
 app.use(cors(corsOptions));
 app.use(morgan("dev"));
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
