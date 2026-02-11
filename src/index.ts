2c9b12a0be9deee57373cc2b6e7f156bd7cb22b3
GAURAB <gaurabkhadka4082@gmail.com>
2026-02-11 09:09:42 +0545

    update image route

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+app.use("/api/v1/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/admin", adminRouter);
