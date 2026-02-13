295bb55a07dcd5f3bd3aa5a14d037c53e6b792bc
GAURAB <gaurabkhadka4082@gmail.com>
2026-02-13 16:56:22 +0545

    Separate app from index

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,6 @@
-import express, { Application } from "express";
-import bodyParser from "body-parser";
-import dotenv from "dotenv";
+import app from "./app";
 import { PORT } from "./config";
-import cors from "cors";
-import morgan from "morgan";
-
-import authRouter from "./routes/auth.route";
-import adminRouter from "./routes/admin/admin.route";
 import { connectDatabase } from "./database/mongodb";
-import path from "path";
-
-dotenv.config();
-const app: Application = express();
-let corsOptions = {
-  origin: ["http://localhost:3000", "http://localhost:3005"],
-  optionsSuccessStatus: 200,
-  credentials: true,
-};
-app.use(cors(corsOptions));
-app.use(morgan("dev"));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use("/api/v1/uploads", express.static(path.join(__dirname, "../uploads")));
-
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/admin", adminRouter);
-
 async function startServer() {
   await connectDatabase();
   app.listen(PORT, () => {
